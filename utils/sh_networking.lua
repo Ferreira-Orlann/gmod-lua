f982dd593d6a579da0a20511d0af1bf5bb7885bf
Ferreira-Orlann <118252904+Ferreira-Orlann@users.noreply.github.com>
2023-02-11 20:07:15 +0100

    Create sh_networking.lua

---
+++ utils/sh_networking.lua
@@ -0,0 +1,29 @@
+snet = snet or {}
+snet.Data = nt.Data or {}
+snet.Receivers = snet.Receivers or {}
+
+snet.BitCount = 4
+
+function snet:Register(name)
+    if SERVER then
+        util.AddNetworkString(name)
+    end
+    self.Data[name] = {}
+    net.Receive(name, function(len, ply)
+        local id = net.ReadUInt(snet.BitCount)
+        if (SERVER) then
+            self.Data[name][id](len - snet.BitCount, ply)
+        else
+            self.Data[name][id](len - snet.BitCount)
+        end
+    end)
+end
+
+function snet:Start(name, id)
+    net.Start(name)
+    net.WriteUInt(id, self.BitCount)
+end
+
+function snet:AddFunc(idname, id, func)
+    self.Data[name][id] = func
+end
